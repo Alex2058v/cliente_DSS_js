db23dd9a6b9923b9ae0dbb463297af004b4411d3
Alex2058v <alexanderayala152@gmail.com>
2023-04-25 22:55:34 -0600

    La funcion de eliminar funciona de manera correcta.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/*La función "cargaDatos()" es una función en JavaScript que utiliza el método "fetch()" para obtener datos de una API RESTful alojada en http://localhost:8000/api/editoriales.
+La solicitud "fetch()", se procesa la respuesta utilizando los métodos "response.json()", que analiza la respuesta JSON de la API, y "then()", que se utiliza para manejar la respuesta una vez que se completa la solicitud "fetch()".
+*/
 function cargaDatos(){
     fetch('http://localhost:8000/api/editoriales')
     .then(response => response.json())
@@ -11,12 +14,34 @@ function cargaDatos(){
                 <td>${edi.contacto}</td>
                 <td>${edi.telefono}</td>
                 <td class="btns">
-                    <a href="modify.php?id=${edi.codigo_editorial}" class="btn btn-secondary">Modificar</a>
+                    <a href="#" class="btn btn-secondary">Modificar</a>
                     <a href="#" class="btn btn-danger" onclick="eliminarDato('${edi.codigo_editorial}')">Eliminar</a>
                 </td>
             `;
             editorialesTable.appendChild(row);
         });
     })
+    //capturar alguna exepcion
     .catch(error => console.error(error));
+}
+
+function eliminarDato(codigoEditorial){
+    //Validar que se la funcion resivio el id
+    if(codigoEditorial){
+        const url = `http://localhost:8000/api/editoriales/${codigoEditorial}`;
+        console.log(url);
+                fetch(url, {
+            method: 'DELETE'
+        })
+            .then(response => {
+                if (response.ok) {
+                    window.location.href = 'index.html';
+                }else {
+                    throw new Error('Ha sucedido un error.');
+                }
+            })
+            .catch(error => {
+                console.error(error);
+        });
+    }
 }
