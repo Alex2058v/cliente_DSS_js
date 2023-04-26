748f593fc7df3d1dd82234ad7aee3f0a6950aa39
Alex2058v <alexanderayala152@gmail.com>
2023-04-26 09:37:53 -0600

    Funcionamiento del ingreso de datos.

---
+++ js/ingresoDatos.js
@@ -0,0 +1,35 @@
+//metodo para el ingreso de editoriales.
+const formulario = document.getElementById('ingresoDatos');
+const guardarBtn = document.getElementById('guardarDatos');
+guardarBtn.addEventListener('click', (event) =>{
+    event.preventDefault();
+
+    const url = `http://localhost:8000/api/editoriales`;
+    //creamos el objeto llamado editorial
+    const editorial = {
+        codigo_editorial: document.getElementById('codigo_editorial').value,
+        nombre_editorial: document.getElementById('nombre_editorial').value,
+        contacto: document.querySelector('#contacto').value,
+        telefono: document.querySelector('#telefono').value
+    };
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(editorial)
+    })
+    .then(response => {
+        if (response.ok) {
+            // Si la respuesta del servidor es exitosa, nos redirigimos a la pagina index.html
+            window.location.href = 'index.html'
+        } else {
+            throw new Error('Ha sucedido un error.');
+        }
+    })
+    .catch(error => {
+        console.error(error);
+        throw new Error('Ha sucedido un error. La respuesta del servidor fue ' + response.status);
+    });
+});
